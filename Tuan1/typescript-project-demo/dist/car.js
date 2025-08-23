85ff9027a974401e72a5b67a5973f9ef5cf054e9
H82M19\Student <Student>
2025-08-23 14:20:23 +0700

    cau3

---
+++ Tuan1/typescript-project-demo/dist/car.js
@@ -0,0 +1,15 @@
+"use strict";
+class Car {
+    constructor(brand, model, year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+    displayInfo() {
+        console.log(`Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}`);
+    }
+}
+// const person = new Person("Alice", 30);
+// person.displayInfo(); // Output:
+const car = new Car("Toyota", "Camry", 2020);
+car.displayInfo(); // Output: Brand: Toyota, Model: Camry, Year:
