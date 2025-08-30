6053f18a30e0b482e973b6f80b7112f01099e8e9
H82M19\Student <Student>
2025-08-30 14:00:29 +0700

    cau9

--- Tuan2/Buoi2/dist/cau9.js
+++ Tuan2/Buoi2/dist/cau9.js
@@ -1,1 +1,13 @@
 "use strict";
+//Write a Promise that reads an array after 1 second and filters even numbers.
+function filterEvenNumbers(arr) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const filtered = arr.filter((num) => num % 2 === 0);
+            resolve(filtered);
+        }, 1000);
+    });
+}
+filterEvenNumbers([1, 2, 3, 4, 5, 6]).then((evenNumbers) => {
+    console.log("Even numbers:", evenNumbers);
+});
