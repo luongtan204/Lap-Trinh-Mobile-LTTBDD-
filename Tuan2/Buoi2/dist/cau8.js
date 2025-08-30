ad969900398a5618a8245b6359b1da3e221b5f2f
H82M19\Student <Student>
2025-08-30 13:55:37 +0700

    cau8

--- Tuan2/Buoi2/dist/cau8.js
+++ Tuan2/Buoi2/dist/cau8.js
@@ -1,1 +1,8 @@
 "use strict";
+Promise.resolve(3)
+    .then((num) => num * num) // square: 4
+    .then((num) => num * 2) // double: 8
+    .then((num) => num + 5) // add 5: 13
+    .then((result) => {
+    console.log("Final result:", result); // Outputs: Final result: 13
+});
